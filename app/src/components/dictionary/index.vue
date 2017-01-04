e4e006c1027988c94ebe1316b1636846739511f7
zx <107939268@qq.com>
2017-01-04 16:48:42 +0800

    增加页面。

--- app/src/components/dictionary/index.vue
+++ app/src/components/dictionary/index.vue
@@ -1,27 +1,76 @@
 <template>
-    <div>
-        <header-component/>
-        <div>this is template body</div>
-        <other-component/>
+  <div class="row">
+    <div class="col-sm-12">
+      <section class="panel">
+        <header class="panel-heading">
+          数据字典列表
+        </header>
+        <div class="panel-body">
+          <div class="table-responsive">
+            <div id="dynamic-table_wrapper" class="dataTables_wrapper form-inline" role="grid">
+              <div class="row-fluid">
+                <div class="search">
+                  <span>字典类别：<input type="text" class="form-control" aria-controls="dynamic-table" name="dataType" id="dataType"></span>
+                  <span>数据字典代码： <input type="text" class="form-control" aria-controls="dynamic-table" name="dataCode" id="dataCode"></span>
+                  <span>字典名称：<input type="text" class="form-control" aria-controls="dynamic-table" name="dataName" id="dataName"></span>
+                  <input class="searchBtn" type="button" value="搜 索">
+                </div>
+
+                <table id="example" class="table table-bordered table-hover">
+                  <thead>
+                  <tr>
+                    <th>字典类别</th>
+                    <th>字典代码</th>
+                    <th>字典名称</th>
+                    <th>银行代码</th>
+                    <th>操作</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr v-for="info in infos">
+                    <td>${info.dataType}</td>
+                    <td>${info.dataCode}</td>
+                    <td>${info.dataName}</td>
+                    <td>${info.bankCode}</td>
+                    <td><a class="btn btn-outline dark btn-xs blue"><i class="glyphicon glyphicon-pencil"></i> 编辑 </a></td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
+  </div>
 </template>
 <style>
-    body{
-        background-color:#ff0000;
-    }
 </style>
 <script>
-    import HeaderComponent from './components/header.vue'
-    import OtherComponent from './components/other.vue'
+  import $ from 'jquery'
     export default{
-        data(){
-            return{
-                msg:'hello vue'
+        data:function(){
+             return {
+              infos: [{
+                dataId: '',
+                dataType: '',
+                dataCode: '',
+                dataName: '',
+                bankCode: ''
+              }],
             }
         },
-        components:{
-            'other-component':OtherComponent,
-            HeaderComponent,
+        methods:{
+         getInfos:function() {
+                var that = this;
+                that.$http.get(QK.SERVER_URL+'', true).then(function (data) {
+                  var data = jQuery.parseJSON(data.body);
+                  var result = QK.getStateCode(that, data.code)
+                  if (result.state) {
+                    that.$set("infos", data.data)
+                  }
+                })
+              }
         }
     }
 </script>
