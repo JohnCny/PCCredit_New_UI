9f5ddb63e7a902202f121f21317cf6d46425a7ff
zhangzhan <zazz1987@163.com>
2017-01-05 13:56:44 +0800

    添加统一常用HTTP错误代码返回页面

--- app/src/components/error.vue
+++ app/src/components/error.vue
@@ -2,12 +2,13 @@
   <div class="wrap">
     <div class="container">
       <div class="leftimg">
-        <img src="/static/images/${errcode}.png" alt="${errcode}-error">
+        <!--<img v-bind:src="/static/images/${errcode}.png" alt="${errcode}-error">-->
+        <span class="errcode">${errcode}</span>
       </div>
       <div class="rightcontent">
         <p style="font: 30px 微软雅黑;">sorry您没有访问权限</p>
         <p style="font: 18px 微软雅黑">您可以通过一下放是继续访问......</p>
-        <div style="width: 273px;height: 60px;background: url(../../../static/images/base.png);color: #ffffff;font:34px 微软雅黑; text-align: center;margin-top: 15px;line-height: 55px" >返回首页</div>
+        <a  v-link="{path: '/system'}">返回首页</a>
       </div>
     </div>
   </div>
@@ -29,10 +30,44 @@
   .leftimg{
     float: left;
     margin-left:0;
+    width: 358px;
+    height: 408px;
+    background: url(/static/images/errorCode.png);
+  }
+  .leftimg .errcode{
+    display: block;
+    margin-top: 45px;
+    margin-left: 20px;
+    width: 220px;
+    height: 70px;
+    color: #fff;
+    text-align: center;
+    font-size: 70px;
+    line-height: 70px;
+    transform:rotate(-10deg);
+    -ms-transform:rotate(-10deg); 	/* IE 9 */
+    -moz-transform:rotate(-10deg); 	/* Firefox */
+    -webkit-transform:rotate(-10deg); /* Safari 和 Chrome */
+    -o-transform:rotate(-10deg); 	/* Opera */
   }
   .rightcontent{
     float: right;
     margin-top: 170px;
+  }
+  .rightcontent>p{
+    color: #fff;
+  }
+  .rightcontent>a{
+    display:block;
+    width: 273px;
+    height: 60px;
+    background: url(/static/images/base.png);
+    color: #ffffff;font:34px 微软雅黑;
+    text-align: center;
+    margin-top: 15px;
+    line-height: 55px;
+    text-decoration: none;
+    cursor: pointer;
   }
 </style>
 <script>
@@ -40,6 +75,11 @@
     data(){
       return {
         errcode: '404'
+      }
+    },
+    route: {
+      canReuse: function () {
+        return false
       }
     },
     ready: function () {
