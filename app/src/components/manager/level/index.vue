1bd8fe8e4afb48bb85adf25cf7dee6fe027b025b
qinwy <qinwuying751@163.com>
2017-01-06 20:04:15 +0800

    客户经理级别

--- app/src/components/manager/level/index.vue
+++ app/src/components/manager/level/index.vue
@@ -1,62 +1,185 @@
+<style src='../../../../static/css/sweetalert.css'></style>
+<style src='../../../../static/css/pageStyle.css'></style>
 <template>
   <div class="row">
     <div class="col-sm-12">
       <section class="panel">
         <header class="panel-heading">
-          客户经理列表
+          客户经理级别列表 <a v-on:click="show" class="btn btn-success btn-sm"><i class="fa fa-plus"></i> 新增</a>
         </header>
         <div class="panel-body">
-          <div class="table-responsive">
-            <table id="customerManagerLevelList" class="table table-bordered">
+          <div class="tableDiv">
+            <table class="table table-striped table-bordered table-hover order-column" id="dtManager">
               <thead>
               <tr>
-                <th>级别名称	</th>
-                <th>对应额度	</th>
-                <th>操作</th>
+                <th>级别名称</th>
+                <th>对应额度</th>
+                <th colspan="2">操作</th>
               </tr>
               </thead>
               <tbody>
               <tr v-for="info in infos">
                 <td>${info.levelName}</td>
                 <td>${info.levelCredit}</td>
-                <td> <a class="btn btn-outline dark btn-xs blue"><i
-                  class="glyphicon glyphicon-pencil"></i> 管理 </a></td>
-                <td><a title="删除" class="btn btn-outline dark btn-xs red delete"><i
-                  class="glyphicon glyphicon-trash"></i> 删除 </a></td>
+                <td><a href="javascript:;" v-on:click="showInfo(info.id)" class="btn btn-warning btn-xs"><i
+                  class="fa fa-edit"></i>
+                  调整 </a></td>
+                <td><a v-on:click="deleteInfo(info.id)" title="删除" class="btn btn-danger btn-xs"><i
+                  class="fa fa-eraser"></i> 删除
+                </a></td>
+
               </tr>
               </tbody>
             </table>
+          </div>
+          <div class="page-bar">
+            <ul>
+              <li v-if="currentpage"><a v-on:click="currentpage--" v-bind:class="{hide:currentpage==1}">上一页</a></li>
+              <li v-for="index in pagenums" v-bind:class="{ active: currentpage == index}">
+                <a v-on:click="pageChange(index)">${index}</a>
+              </li>
+              <li v-if="currentpage!=totlepage"><a v-on:click="currentpage++">下一页</a></li>
+              <li><a>共<i>${totlepage}</i>页</a></li>
+            </ul>
           </div>
         </div>
       </section>
     </div>
   </div>
 </template>
-<style>
+<style scoped>
 </style>
 <script>
-  import $ from 'jquery'
-    export default{
-        data:function(){
-             return {
-              infos: [{
-                id: '',
-                levelName: '',
-                levelCredit: ''
-              }],
-            }
+  import QK from '../../../QK'
+  import swal from 'sweetalert'
+  export default{
+    data: function () {
+      return {
+        infos: {
+          id: '',
+          levelName: '',
+          levelCredit: ''
         },
-        methods:{
-         getInfos:function() {
-                var that = this;
-                that.$http.get(QK.SERVER_URL+'', true).then(function (data) {
-                  var data = jQuery.parseJSON(data.body);
-                  var result = QK.getStateCode(that, data.code)
-                  if (result.state) {
-                    that.$set("infos", data.data)
-                  }
-                })
-              }
+        currentpage: 1,//第几页
+        totlepage: '',//共几页
+        visiblepage: 10,//隐藏10页
+      }
+    },
+    components: {
+
+    },
+    ready: function () {
+      this.init()
+    },
+    computed: {
+      pagenums: function () {
+        //初始化前后页边界
+        var lowPage = 1;
+        var highPage = this.totlepage;
+        var pageArr = [];
+        if (this.totlepage > this.visiblepage) {//总页数超过可见页数时，进一步处理；
+          var subVisiblePage = Math.ceil(this.visiblepage / 2);
+          if (this.currentpage > subVisiblePage && this.currentpage < this.totlepage - subVisiblePage + 1) {//处理正常的分页
+            lowPage = this.currentpage - subVisiblePage;
+            highPage = this.currentpage + subVisiblePage - 1;
+          } else if (this.currentpage <= subVisiblePage) {//处理前几页的逻辑
+            lowPage = 1;
+            highPage = this.visiblepage;
+          } else {//处理后几页的逻辑
+            lowPage = this.totlepage - this.visiblepage + 1;
+            highPage = this.totlepage;
+          }
         }
+        //确定了上下page边界后，要准备压入数组中了
+        while (lowPage <= highPage) {
+          pageArr.push(lowPage);
+          lowPage++;
+        }
+        return pageArr;
+      },
+    },
+    watch: {
+      currentpage: function (oldValue, newValue) {
+        this.init()
+      }
+    },
+    methods: {
+      init: function () {
+        var that = this
+        var search = 'start=' + that.currentpage + '&&length=' + this.visiblepage
+        that.$http.get(QK.SERVER_URL + '/api/customerManagerLevel/pageList?' + search, true).then(function (res) {
+          var data = jQuery.parseJSON(res.body)
+          var page = parseInt(data.recordsTotal / 10);
+          if (data.recordsTotal % 10) {
+            page = page + 1;
+          }
+          that.$set('totlepage', page)
+          that.$set('infos', data.data)
+        })
+      },
+      pageChange: function (page) {
+        page = page || 1
+        var that = this
+        if (that.currentpage != page) {
+          that.currentpage = page
+        }
+      },
+      showInfo: function (id) {
+        //记录当前地址
+        QK.noteNowUrl()
+        //跳转地址
+        this.$router.go({path: '/system/managerLevel/edit/' + id})
+      },
+      show: function () {
+        //记录当前地址
+        QK.noteNowUrl()
+        //跳转地址
+        this.$router.go({path: '/system/managerLevel/new'})
+      },
+      deleteInfo: function (id) {
+        var that = this
+        swal({
+            title: "你确定要删除这条信息吗?",
+            text: "删除无法后将无法撤销！",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#EF5350",
+            confirmButtonText: "确定!",
+            cancelButtonText: "取消",
+            closeOnConfirm: false,
+            closeOnCancel: false
+          },
+          function (isConfirm) {
+            if (isConfirm) {
+              swal({
+                  title: "删除!",
+                  text: "您的文件已被删除！",
+                  confirmButtonColor: "#66BB6A",
+                  type: "success"
+                },
+                function () {
+                  that.$http.delete(QK.SERVER_URL + '/api/customerManagerLevel/' + id).then(function (data) {
+                    var data = jQuery.parseJSON(data.body)
+                    var result = QK.getStateCode(that, data.code)
+                    if (result.state) {
+                      that.infos.$remove(that.infos.find(t => t.id === id))
+                      //document.location.reload();
+                    }
+                  }, function (error) {
+                    console.log(error)
+                  })
+                });
+            } else {
+              swal({
+                title: "取消",
+                text: "您的文件是安全的！",
+                confirmButtonColor: "#2196F3",
+                type: "error"
+              });
+            }
+          });
+      },
     }
+  }
+
 </script>
--- app/src/components/manager/level/new.vue
+++ app/src/components/manager/level/new.vue
@@ -1,27 +1,191 @@
 <template>
-    <div>
-        <header-component/>
-        <div>this is template body</div>
-        <other-component/>
+  <div class="row">
+    <div class="col-sm-12">
+      <section class="panel">
+        <header class="panel-heading">
+          客户经理级别信息
+        </header>
+        <div class="panel-body">
+          <form action="" id="managerLevelEdit" @submit.prevent="handleSubmit">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="levelName">级别名称</label>
+                  <div class="input-icon right">
+                    <input v-model="infos.levelName" id="levelName" type="text" class="form-control" name="levelName" placeholder="请输入级别名称">
+                    <div class="message">${errors.levelNameError}</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div id="nameDiv" class="form-group">
+                  <label for="levelCredit">对应额度</label>
+                  <div class="input-icon right">
+                    <input  v-model="infos.levelCredit" id="levelCredit" type="text" class="form-control" name="levelCredit" placeholder="请输入对应额度">
+                    <div class="message">${errors.levelCreditError}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="overdueTolerateRate">逾期容忍率</label>
+                  <div class="input-icon right">
+                    <input  v-model="infos.overdueTolerateRate" id="overdueTolerateRate" type="text" class="form-control" name="overdueTolerateRate" placeholder="请输入10-99之间的年龄值" >
+                    <div class="message">${errors.overdueTolerateRateError}</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="badloanTolerateRate">不良容忍率</label>
+                  <div class="input-icon right">
+                    <input  v-model="infos.badloanTolerateRate" id="badloanTolerateRate" type="text" class="form-control" name="badloanTolerateRate" placeholder="请输入10-99之间的年龄值" >
+                    <div class="message">${errors.badloanTolerateRateError}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="postloanMonitorTolerateNum">贷后监控容忍量</label>
+                  <div class="input-icon right">
+                    <input  v-model="infos.postloanMonitorTolerateNum" id="postloanMonitorTolerateNum" type="text" class="form-control" name="postloanMonitorTolerateNum" placeholder="请正确填写您的电话号码">
+                    <div class="message">${errors.postloanMonitorTolerateNumError}</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="loanLimitMin">该级别贷款最小额度</label>
+                  <div class="input-icon right">
+                    <input v-model="infos.loanLimitMin" id="loanLimitMin" type="text" class="form-control" name="loanLimitMin" placeholder="请输入正确的身份证号" >
+                    <div class="message">${errors.loanLimitMinError}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="loanNumberMin">该级别贷款最小笔数</label>
+                  <div class="input-icon right">
+                    <input v-model="infos.loanNumberMin" id="loanNumberMin" type="text" class="form-control" name="loanNumberMin" placeholder="请输入合法的邮件地址">
+                    <div class="message">${errors.loanNumberMinError}</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="overdueRateMin">该级别最小逾期率</label>
+                  <div class="input-icon right">
+                    <input v-model="infos.overdueRateMin" id="overdueRateMin" type="text" class="form-control" name="overdueRateMin" placeholder="请输入合法的邮件地址">
+                    <div class="message">${errors.overdueRateMinError}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class=row>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="badloanRateMin">该级别最小不良率</label>
+                  <div class="input-icon right">
+                    <input v-model="infos.badloanRateMin" id="badloanRateMin" type="text" class="form-control" name="badloanRateMin" placeholder="请输入合法的邮件地址">
+                    <div class="message">${errors.badloanRateMinError}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <button id="btn_submit" class="btn btn-success">确定</button>
+                <a href="" type="reset" class="btn btn-default">取消</a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
     </div>
+  </div>
 </template>
-<style>
-    body{
-        background-color:#ff0000;
-    }
+<style scoped>
+  #userNew input,#userNew select{
+    width:80%
+  }
+  .message{
+    color:#a94442;
+    height:20px;
+  }
 </style>
 <script>
-    import HeaderComponent from './components/header.vue'
-    import OtherComponent from './components/other.vue'
+    import QK from '../../../QK'
+    import jQueryValidation from 'jquery-validation'
     export default{
-        data(){
-            return{
-                msg:'hello vue'
-            }
+        data:function(){
+           return {
+                infos:{
+                  badloanRateMin: '',
+                  badloanTolerateRate: '',
+                  levelCredit: '',
+                  levelName: '',
+                  loanLimitMin: '',
+                  loanNumberMin: '',
+                  overdueRateMin: '',
+                  overdueTolerateRate: '',
+                  postloanMonitorTolerateNum: ''
+                },
+                errors:{
+                  badloanRateMinErrors: '',
+                  badloanTolerateRateErrors: '',
+                  idErrors: '',
+                  levelCreditErrors: '',
+                  levelNameErrors: '',
+                  loanLimitMinErrors: '',
+                  loanNumberMinErrors: '',
+                  overdueRateMinErrors: '',
+                  overdueTolerateRateErrors: '',
+                  postloanMonitorTolerateNumErrors: ''
+                }
+           }
         },
-        components:{
-            'other-component':OtherComponent,
-            HeaderComponent,
+        ready:function(){
+          QK.addMethod()
+        },
+        methods:{
+        handleSubmit () {
+            var that = this
+            var bool = QK.formValidation({
+              id: "#managerLevelEdit",
+              rulesMap:{
+                  badloanRateMin:{required: !0,isIntGtZero: !0},
+                  badloanTolerateRate:{required: !0,isIntGtZero: !0},
+                  levelCredit:{required: !0,isIntGtZero: !0},
+                  levelName:{required: !0},
+                  loanLimitMin:{required: !0,isIntGtZero: !0},
+                  loanNumberMin:{required: !0,isIntGtZero: !0},
+                  overdueRateMin:{required: !0,isIntGtZero: !0},
+                  overdueTolerateRate:{required: !0,isIntGtZero: !0},
+                  postloanMonitorTolerateNum:{required: !0,isIntGtZero: !0},
+              }
+            })
+            //验证结果  true  false
+            if(bool){
+              that.infos.levelName = that.infos.levelName+''
+              that.$http.post(QK.SERVER_URL+'/api/customerManagerLevel', that.infos, true).then(function (data) {
+                var data = jQuery.parseJSON(data.body)
+                var result = QK.getStateCode(that,data.code)
+                if (result.state) {
+                  alert("创建成功")
+                  that.$router.go({path:"/system/managerLevel/list"})
+                }else{
+                  alert("创建失败")
+                }
+              })
+            }
+            return false
+          },
         }
     }
 </script>
