f2b2f7c268575069439a86d551a359edde23823a
qinwy <qinwuying751@163.com>
2017-02-09 17:50:46 +0800

    ipc整体前端架构设计

--- app/src/components/application/tables/jcjy.vue
+++ app/src/components/application/tables/jcjy.vue
@@ -1,25 +1,41 @@
 <template>
+  <table class="table table-bordered" id="jcjyTable">
+    <thead>
+      交叉检验
+    </thead>
 
+  </table>
 </template>
 <style scope>
-
 </style>
 <script>
-  import QK from '../../../QK'
-  export default{
-    data(){
-      return{
-        vars:{}
-      }
-    },
-    ready :function(){
-      this.init()
-    },
-    methods:{
-      init:function() {
-
-      },
+import QK from '../../../QK'
+export default{
+  data(){
+    return{
+      vars:[],
     }
+  },
+  ready :function(){
+
+  },
+  created: function () {
+    QK.vector.$on('getfromchild', this.init)
+  },
+  beforeDestroy: function () {
+    QK.vector.$off('getfromchild', this.init)
+  },
+  methods: {
+    init:function(varsArr) {
+      var that = this
+      var menuid = $("#menu1").find("li.active").data("menuid")
+      $(varsArr).each(function(i,v){
+        if($(v)[0].groupId == menuid){
+          that.$set("vars",$(v))
+        }
+      })
+    },
   }
+}
 
 </script>
